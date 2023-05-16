5828d256dad579c0e64abcdeacf15333deef32c7
Najee Aleem <Naleem25@gmail.com>
2023-05-16 13:49:24 -0500

    jdbc complete

--- src/main/java/Controllers/CreateAdServlet.java
+++ src/main/java/Controllers/CreateAdServlet.java
@@ -1,11 +1,17 @@
+package Controllers;
+
+import dao.DaoFactory;
+import models.Ad;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
-@WebServlet(name = "CreateAdServlet", urlPatterns = "/ads/create")
+@WebServlet(name = "Controller.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
@@ -14,11 +20,15 @@ public class CreateAdServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
+            1, // for now, we'll hardcode the user id
             request.getParameter("title"),
             request.getParameter("description")
         );
-        DaoFactory.getAdsDao().insert(ad);
+        try {
+            DaoFactory.getAdsDao().insert(ad);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         response.sendRedirect("/ads");
     }
 }
